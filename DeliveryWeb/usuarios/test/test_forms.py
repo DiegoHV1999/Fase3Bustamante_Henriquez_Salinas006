245ad144655c1178ff9b6a1c176d997b6e07b7e4
jose bustamante <bustamante.maripan@gmail.com>
2020-12-02 19:10:30 -0300

    DeliveryWeb: Entrega fase 3

--- DeliveryWeb/usuarios/test/test_forms.py
+++ DeliveryWeb/usuarios/test/test_forms.py
@@ -2,8 +2,13 @@ from django.test import TestCase
 from django.urls import reverse
 from usuarios.models import local, transporte
 
-def test_get_template_reverse(self):
+def test_get_template_reverse_local(self):
         
-        local = local.objects.create(nombre=sushi, direccion="esmeralda")
+        local = local.objects.create(nombre="sushi", direccion="esmeralda")
         response = self.client.get(reverse('local_detail', kwargs={'pk':local.pk}))
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
+
+def test_get_template_reverse_transporte(self):
+        transporte = transporte.objects.create(nombre="didi", telefono="975525786")
+        response = self.client.get(reverse('transporte_detail', kwars={'pk':transporte.pk}))
+        self.assertEqual(response.status_code, 200)
